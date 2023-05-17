b5793b09b55067fcc7ac83f42302ba0f9ea04e09
Shreyansh Jain <jshreyansh87@gmail.com>
2023-05-18 00:06:23 +0530

    Install router and separate app components

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,13 @@
 import './App.css';
+import Post from './Components/Post';
+import NavBar from './Components/NavBar';
+import PostList from './Components/PostList';
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="#" className="logo">BlogDose</a>
-        <nav>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
-        </nav>
-      </header>
-
-      <div className="posts-list">
-
-        <div className="post">
-          <div className="post-image">
-            <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="post-image" />
-          </div>
-          <div className="post-content">
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in fugit eius.</h2>
-            <p className="post-content-info">
-              <a href="#" className="author">Shreyansh Jain</a>
-              <time>2023-05-17 23:43</time>
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptates explicabo qui dolorum temporibus iusto doloremque! Soluta minima est dolores explicabo non expedita illum neque, perspiciatis animi tenetur rerum esse nesciunt aperiam velit magnam!</p>
-          </div>
-        </div>
-
-        <div className="post">
-          <div className="post-image">
-            <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="post-image" />
-          </div>
-          <div className="post-content">
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in fugit eius.</h2>
-            <p className="post-content-info">
-              <a href="#" className="author">Shreyansh Jain</a>
-              <time>2023-05-17 23:43</time>
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptates explicabo qui dolorum temporibus iusto doloremque! Soluta minima est dolores explicabo non expedita illum neque, perspiciatis animi tenetur rerum esse nesciunt aperiam velit magnam!</p>
-          </div>
-        </div>
-
-        <div className="post">
-          <div className="post-image">
-            <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="post-image" />
-          </div>
-          <div className="post-content">
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in fugit eius.</h2>
-            <p className="post-content-info">
-              <a href="#" className="author">Shreyansh Jain</a>
-              <time>2023-05-17 23:43</time>
-            </p>
-            <p className="post-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptates explicabo qui dolorum temporibus iusto doloremque! Soluta minima est dolores explicabo non expedita illum neque, perspiciatis animi tenetur rerum esse nesciunt aperiam velit magnam!</p>
-          </div>
-        </div>
-
-      </div>
+      <NavBar />
+      <PostList />
     </main>
   );
 }
---
+++ client/src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>
+            <a href="#" className="logo">BlogDose</a>
+            <nav>
+                <a href="#">Login</a>
+                <a href="#">Register</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
